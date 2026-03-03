de6a1f8fbfb39f3df2b45b73bbcd7e7afad730cf
jahonov <asilbekjahonov2001@gmail.com>
2026-03-03 16:44:13 +0500

    updated command handling to support string identifiers in VeonPrebidReactNativeViewManager

--- android/src/main/java/com/setupadprebidreactnative/VeonPrebidReactNativeViewManager.kt
+++ android/src/main/java/com/setupadprebidreactnative/VeonPrebidReactNativeViewManager.kt
@@ -109,16 +109,16 @@ class VeonPrebidReactNativeViewManager(
     ) {
         Log.d(TAG, "Received command (String): $commandId")
 
-        when (commandId.toIntOrNull()) {
-            COMMAND_LOAD_BANNER -> view.loadBanner()
-            COMMAND_SHOW_BANNER -> view.showBanner()
-            COMMAND_HIDE_BANNER -> view.hideBanner()
-            COMMAND_LOAD_INTERSTITIAL -> view.loadInterstitial()
-            COMMAND_SHOW_INTERSTITIAL -> view.showInterstitial()
-            COMMAND_HIDE_INTERSTITIAL -> view.hideInterstitial()
-            COMMAND_PAUSE_AUCTION -> view.pauseAuction()
-            COMMAND_RESUME_AUCTION -> view.resumeAuction()
-            COMMAND_DESTROY_AUCTION -> view.destroyAuction()
+        when (commandId) {
+            "loadBanner", COMMAND_LOAD_BANNER.toString() -> view.loadBanner()
+            "showBanner", COMMAND_SHOW_BANNER.toString() -> view.showBanner()
+            "hideBanner", COMMAND_HIDE_BANNER.toString() -> view.hideBanner()
+            "loadInterstitial", COMMAND_LOAD_INTERSTITIAL.toString() -> view.loadInterstitial()
+            "showInterstitial", COMMAND_SHOW_INTERSTITIAL.toString() -> view.showInterstitial()
+            "hideInterstitial", COMMAND_HIDE_INTERSTITIAL.toString() -> view.hideInterstitial()
+            "pauseAuction", COMMAND_PAUSE_AUCTION.toString() -> view.pauseAuction()
+            "resumeAuction", COMMAND_RESUME_AUCTION.toString() -> view.resumeAuction()
+            "destroyAuction", COMMAND_DESTROY_AUCTION.toString() -> view.destroyAuction()
             else -> Log.w(TAG, "Unknown command: $commandId")
         }
     }
